ccfd61b326e7febb636b0071d6bf2d209ff15daf
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-09-22 11:01:57 -0700

    added weather get

---
+++ src/Components/Forecast.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+
+export default class Forecast extends Component{
+  render() {
+    return (
+    <Container>
+        {/* {this.props.weather} */}
+    </Container>
+    );
+  }
+}
