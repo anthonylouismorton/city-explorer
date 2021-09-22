e159b65b01c5b0f4c1531b5e8a5ac87445642952
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-09-22 11:06:18 -0700

    removed row

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+
 
 
 export default class Forecast extends Component{
