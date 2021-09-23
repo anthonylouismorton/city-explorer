aae52df9aa5c5ef72cbf95e1a7b0ddf020c0a47e
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-09-23 15:19:46 -0700

    added image to read me

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -1,14 +1,26 @@
 import { Component } from 'react';
+import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
+
 
 
 
 export default class Forecast extends Component{
   render() {
     return (
-    <Container>
-        {/* {this.props.weather} */}
-    </Container>
+      <Container className="weatherReport">
+        {this.props.weather.map( (weatherData,idx) => {
+          return(
+          
+          <div key={idx}>
+            <Card.Title>{weatherData.datetime}</Card.Title>
+            console.log(weatherData.datetime);
+            <Card.Text>{weatherData.description}</Card.Text>
+          </div>
+          
+          );
+        })}
+     </Container>
     );
   }
 }
